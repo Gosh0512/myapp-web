5d0adbc42b3e20d894307c64e3c66717311a80cf
DESKTOP <purpleduck5@naver.com>
2025-06-09 19:09:40 +0900

    commit test

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="min-h-screen bg-gradient-to-r from-blue-500 to-purple-600 flex flex-col items-center justify-center text-white">
     <h1 class="text-4xl font-bold mb-4">Tailwind CSS 적용 성공!</h1>
-    <p class="text-lg">Vue + Tailwind CSS 프로젝트입니다 🎉</p>
+    <p class="text-lg">GOSH0512 BLOG 준비중</p>
     <button class="mt-6 px-6 py-2 bg-white text-blue-600 rounded-lg shadow-md hover:bg-gray-100 transition">
-      클릭해보세요
+      CLICK
     </button>
   </div>
 </template>
