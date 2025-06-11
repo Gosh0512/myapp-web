bcdaf624664185ac1b5e7706d12023c34fd1ecc4
DESKTOP <purpleduck5@naver.com>
2025-06-11 22:56:08 +0900

    페이지수정2

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Profile from '../views/Profile.vue'
-import task from '../views/task.vue'
+import Task from '../views/task.vue'
 import Investment from '../views/Investment.vue'
 import Reading from '../views/Reading.vue'
 import Workout from '../views/Workout.vue'
@@ -9,7 +9,7 @@ import Fishing from '../views/Fishing.vue'
 const routes = [
   { path: '/', redirect: '/profile' },
   { path: '/profile', component: Profile },
-  { path: '/task', component: task },
+  { path: '/task', component: Task },
   { path: '/investment', component: Investment },
   { path: '/reading', component: Reading },
   { path: '/workout', component: Workout },
